e6b55df4e72beb1ae5f36e9f0a0addc67607071c
jkla23 <654358638@qq.com>
2019-02-28 09:01:00 +0800

    router权限控制

--- vuepro/src/components/Error401.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div style="padding:100px">
-    <h2 style="font-size:30px">您没有权限访问该页面</h2>
-  </div>
-</template>
--- vuepro/src/components/Error404.vue
+++
@@ -1,5 +0,0 @@
-<template lang="">
-    <div style="padding:100px">
-        <h2 style="font-size:30px">您访问的页面不存在</h2>
-    </div>
-</template>
